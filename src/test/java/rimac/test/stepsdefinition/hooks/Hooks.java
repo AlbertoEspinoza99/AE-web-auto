ebe76b1332d2a005a781c08fe9555fd77c561609
AlbertoEspinoza99 <rodolfoalejos4569@gmail.com>
2025-06-09 00:27:35 -0500

    commit

--- src/test/java/rimac/test/stepsdefinition/hooks/Hooks.java
+++ src/test/java/rimac/test/stepsdefinition/hooks/Hooks.java
@@ -27,11 +27,13 @@ import static rimac.test.utils.Constantes.USERNAME;
 
 public class Hooks {
 
+    // Se define un actor global que representará al usuario en cada prueba
     public static final Actor actor = Actor.named("Usuario");
 
     @Before
     public void setTheStage(Scenario scenario) {
 
+        // Se obtienen las variables de entorno definidas en serenity.conf
         EnvironmentVariables variables = SystemEnvironmentVariables.createEnvironmentVariables();
 
         String ambiente;
@@ -78,9 +80,11 @@ public class Hooks {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("--window-size=1920,1080");
-//         options.addArguments("--headless=new");
+        // Se puede activar para ejecución sin interfaz gráfica
+        // options.addArguments("--headless=new");
 
 
+        // Prepara el WebDriver de Chrome
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
         // Inicializar el actor y navegador
@@ -90,10 +94,10 @@ public class Hooks {
 
 
 
-        OnStage.setTheStage(new OnlineCast());
-        actor.can(BrowseTheWeb.with(driver));
+        OnStage.setTheStage(new OnlineCast()); // <--- Inicializa el escenario de Screenplay con una "compañía" de actores
+        actor.can(BrowseTheWeb.with(driver));  // <---  El actor puede navegar en la web usando el driver configurado
         actor.attemptsTo(Open.url(url));  // <--- Usar la instancia con URL configurada
-        getDriver().manage().window().maximize();
+        getDriver().manage().window().maximize();  //  <--- Maximiza la ventana del navegador
 
         // Guardar credenciales en la memoria del actor
         actor.remember(USERNAME, username);
@@ -102,6 +106,7 @@ public class Hooks {
 
     @After
     public void tearDown() {
+        // Esta línea detiene y limpia el entorno de los actores de Screenplay.
         OnStage.drawTheCurtain();
     }
 
